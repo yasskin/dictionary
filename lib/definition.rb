9e7c6f3bf474b77b66f51ccaaba8d4436cc704eb
Noah <yasskin@icloud.com>
2016-07-08 09:06:14 -0700

    add working custom Definition object constructor with passing definition_spec

---
+++ lib/definition.rb
@@ -0,0 +1,8 @@
+class Definition
+  attr_reader :definition_entry
+
+  def initialize(attributes)
+    @definition_entry = attributes[:definition_entry]
+  end
+
+end
--- spec/definition_spec.rb
+++ spec/definition_spec.rb
@@ -1,12 +1,13 @@
-# require('rspec')
-# require('definition')
-#
-# describe('Definition') do
-#
-#   describe('Definition#initialize') do
-#     it("should return a single definition from a newly created Definition object") do
-#     definition_entry_one = Definition.
-#     end
-#   end
-#
-# end
+require('rspec')
+require('definition')
+
+describe('Definition') do
+
+  describe('Definition#initialize') do
+    it("should return a single definition from a newly created Definition object") do
+      new_ruby_definition_one = Definition.new({definition_entry: "A dynamic, open source programming language with a focus on simplicity and productivity."})
+      expect(new_ruby_definition_one.definition_entry()).to(eq("A dynamic, open source programming language with a focus on simplicity and productivity."))
+    end
+  end
+
+end
