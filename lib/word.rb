03dc025644abe5300b726ee1026f657d1f4a7765
Noah <yasskin@icloud.com>
2016-07-08 09:28:39 -0700

    add new function that adds multiple definitions to a Word object with a passing word_spec test

--- lib/word.rb
+++ lib/word.rb
@@ -1,20 +1,35 @@
 class Word
-  attr_reader :word_entry, :definitions
+  attr_reader :word_entry, :definition_entries
 
-  @@entry_words = []
+  @@word_entries = []
 
   def self.clear()
-    @@entry_words = []
+    @@word_entries = []
   end
 
   def self.contacts()
-    @@entry_words
+    @@word_entries
   end
 
   def initialize(attributes)
     @word_entry = attributes[:word_entry]
-    @all_definitions = []
-    @@entry_words.push(self)
+    @definition_entries = []
+    @id = @@word_entries.length + 1
+    @@word_entries.push(self)
+  end
+
+  def add_definition(definition_entry)
+    @definition_entries.push(definition_entry)
+  end
+
+  define_singleton_method(:find) do |id|
+    found_word = nil
+    @@word_entries.each() do |word|
+      if word_entry.id().eq?(id)
+        found_word = word
+      end
+    end
+    found_word
   end
 
 end
--- spec/word_spec.rb
+++ spec/word_spec.rb
@@ -12,12 +12,16 @@ describe('Word') do
       expect(new_word.word_entry()).to(eq("Ruby"))
     end
   end
-  #
-  # describe("Word#definitions") do
-  #   it("should return a definition object") do
-  #     new_word = Word.new({entry_word: "Ruby"})
-  #     new_definition_entry_one = Definition.new({entry_definition: "A dynamic, open source programming language with a focus on simplicity and productivity."})
-  #     expect(new_definition_entry_one.entry_definition()).to(eq("A dynamic, open source programming language with a focus on simplicity and productivity."))
-  # end
+
+  describe("Word#definitions") do
+    it("should return a definition entry with an array that has two definitions") do
+      new_word = Word.new({entry_word: "Ruby"})
+      new_definition_entry_one = Definition.new({entry_definition: "A dynamic, open source programming language with a focus on simplicity and productivity."})
+      new_definition_entry_two = Definition.new({entry_definition: "Ruby blends parts of other languages (Perl, Smalltalk, Eiffel, Ada, and Lisp) to form a new language that balances functional programming with imperative programming."})
+      new_word.add_definition(new_definition_entry_one)
+      new_word.add_definition(new_definition_entry_two)
+      expect(new_word.definition_entries()).to(eq([new_definition_entry_one, new_definition_entry_two]))
+    end
+  end
 
 end
